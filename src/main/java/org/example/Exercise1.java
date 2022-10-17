8d8856a0dfc7e6527a5d6c271cd2a005882f207d
Farhad Towfighian <fartow@gmail.com>
2022-10-17 15:00:20 +0200

    first exercise done.

---
+++ src/main/java/org/example/Exercise1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        int[] myNum = {10, 20, 30, 40};
+        int size = myNum.length;
+        for (int i = 0; i < size; i++){
+            System.out.println(myNum [i]);
+
+        }
+
+    }
+}
+
